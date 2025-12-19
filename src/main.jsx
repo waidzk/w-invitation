316ef8bab7d08f3ccf5e6115439aff87c11bbbf0
Pradhityas Waidz Kamal <57702954+waidzk@users.noreply.github.com>
2025-12-19 14:16:07 +0700

    Add metadata and change title

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Novar & Raras Wedding Invitation',
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
